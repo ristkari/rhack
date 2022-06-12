d5e6b8ac78f3bbd6dbad5e7dff3eb97c7224144b
Aki Ristkari <aki.ristkari@reaktor.com>
2022-06-12 23:06:59 +0300

    mod decrypt

--- decrypt.sh
+++ decrypt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ENCRYPTED_FILES=`find . -type f -regex ".*\.enc\(\.yaml\|\.json\)?\$"`
+ENCRYPTED_FILES=`find . -type f -regex ".*\.enc\(\.yaml\|\.json\|\.ini\)?\$"`
 for FILE in ${ENCRYPTED_FILES}; do
   DECRYPTED_FILE=`echo "$FILE" | sed 's/.enc././g'`
   echo "Decrypting $FILE"
