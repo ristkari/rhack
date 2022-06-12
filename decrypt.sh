b8b1ca03360574b4afb2c18a8e1ccea372ed97ed
Aki Ristkari <aki.ristkari@reaktor.com>
2022-06-12 22:29:18 +0300

    add decrypt

---
+++ decrypt.sh
@@ -0,0 +1,9 @@
+
+#!/bin/sh
+ENCRYPTED_FILES=`find . -type f -regex ".*\.enc\(\.yaml\|\.json\)?\$"`
+for FILE in ${ENCRYPTED_FILES}; do
+  DECRYPTED_FILE=`echo "$FILE" | sed 's/.enc././g'`
+  echo "Decrypting $FILE"
+  sops --decrypt $FILE > $DECRYPTED_FILE
+done
+
