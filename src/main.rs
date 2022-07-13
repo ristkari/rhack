5c40f6240ebca66c2a748510fcecd0f216a77bf4
Aki Ristkari <aki@ristkari.fi>
2022-07-13 08:09:42 +0300

    changes

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 mod redis_datastore;
 
 extern crate redis;
+use rand::{distributions::Alphanumeric, Rng};
 use redis_datastore::connection_manager;
 use serde::{Deserialize, Serialize};
 use serde_json::Result as sj;
 use std::{error::Error, time::Duration};
 use tokio::time::sleep;
+use ulid::Ulid;
 
 use redis::{
     from_redis_value,
@@ -35,6 +37,35 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+async fn do_redis_stuff() -> Result<(), Box<dyn Error>> {
+    let mut my_con = match redis_datastore::connection_manager::get_connection_manager().await {
+        Ok(res) => res.clone(),
+        Err(e) => {
+            //println!("{:?}", e);
+            println!("{}", e);
+            panic!("oh noes");
+        }
+    };
+
+    let p = Person {
+        name: rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect(),
+        age: 30,
+        phones: vec!["123-456-7890".to_string(), "123-456-7891".to_string()],
+    };
+
+    let _: () = redis::pipe()
+        .atomic()
+        .set(Ulid::new().to_string(), serde_json::to_string(&p).unwrap())
+        .expire("pylly", 600)
+        .query_async(&mut my_con)
+        .await?;
+    Ok(())
+}
+
 #[get("/person")]
 async fn person(req_body: String) -> impl Responder {
     let p = Person {
@@ -42,6 +73,7 @@ async fn person(req_body: String) -> impl Responder {
         age: 30,
         phones: vec!["123-456-7890".to_string(), "123-456-7891".to_string()],
     };
+    do_redis_stuff().await;
     HttpResponse::Ok().body(serde_json::to_string(&p).unwrap())
 }
 
@@ -58,42 +90,6 @@ struct Person {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    //let cm = redis_datastore::connection_manager::get_connection_manager().await;
-    let mut my_con = match redis_datastore::connection_manager::get_connection_manager().await {
-        Ok(res) => {
-            println!("{:?}", "Got Connection Manager");
-            res.clone()
-        }
-        Err(e) => {
-            //println!("{:?}", e);
-            println!("{}", e);
-            panic!("oh noes");
-        }
-    };
-
-    let p = Person {
-        name: "John".to_string(),
-        age: 30,
-        phones: vec!["123-456-7890".to_string(), "123-456-7891".to_string()],
-    };
-
-    let _: () = redis::pipe()
-        .atomic()
-        .set("pylly", serde_json::to_string(&p).unwrap())
-        .expire("pylly", 600)
-        .query_async(&mut my_con)
-        .await?;
-
-    /*
-    let shared_config = aws_config::load_from_env().await;
-    let client = Client::new(&shared_config);
-    let req = client.list_tables().limit(10);
-    {
-        let resp = req.send().await.unwrap();
-        println!("Current DynamoDB tables: {:?}", resp.table_names);
-    }
-    */
-
     HttpServer::new(|| {
         App::new()
             .service(hello)
@@ -101,6 +97,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .service(person)
             .route("/hey", web::get().to(manual_hello))
     })
+    .workers(4)
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
